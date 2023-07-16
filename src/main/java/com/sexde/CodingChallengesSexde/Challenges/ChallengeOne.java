8504e2893384eb25bad9cabb9c64ff4fd36e799d
Álvaro Berdote <avberdote@hotmail.com>
2023-07-16 23:35:40 +0200

    fix: remove list

--- src/main/java/com/sexde/CodingChallengesSexde/Challenges/ChallengeOne.java
+++ src/main/java/com/sexde/CodingChallengesSexde/Challenges/ChallengeOne.java
@@ -18,18 +18,7 @@ public final class ChallengeOne {
         return
                 IntStream
                         .range(1, numbers.size())
-                        .mapToObj(i ->
-                                List.of(
-                                        numbers.get(i - 1),
-                                        numbers.get(i)
-                                )
-                        )
-                        .map(pairList ->
-                                pairList
-                                        .stream()
-                                        .mapToInt(Integer::intValue)
-                                        .sum()
-                        )
+                        .mapToObj(i -> numbers.get(i - 1) + numbers.get(i))
                         .max(Integer::compareTo)
                         .orElseThrow(() -> new InvalidParameterException("Cannot operate with items whose size is less than two."));
 
