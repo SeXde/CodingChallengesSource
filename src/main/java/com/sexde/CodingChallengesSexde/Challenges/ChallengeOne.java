aeea3f391124f390f7705504419380833c35a8fa
Álvaro Berdote <avberdote@hotmail.com>
2023-07-19 00:53:49 +0200

    feat: update ChallengeOne

--- src/main/java/com/sexde/CodingChallengesSexde/Challenges/ChallengeOne.java
+++ src/main/java/com/sexde/CodingChallengesSexde/Challenges/ChallengeOne.java
@@ -2,7 +2,7 @@ package com.sexde.CodingChallengesSexde.Challenges;
 
 import java.security.InvalidParameterException;
 import java.util.List;
-import java.util.Objects;
+import java.util.Optional;
 import java.util.stream.IntStream;
 
 public final class ChallengeOne {
@@ -11,34 +11,32 @@ public final class ChallengeOne {
 
     public static Integer getMaxSumOfAdjacentNumbers(final List<Integer> numbers) {
 
-        if (Objects.isNull(numbers)) {
-            throw new InvalidParameterException("Number list cannot be null.");
-        }
-
         return
-                IntStream
-                        .range(1, numbers.size())
-                        .mapToObj(i -> numbers.get(i - 1) + numbers.get(i))
-                        .max(Integer::compareTo)
-                        .orElseThrow(() -> new InvalidParameterException("Cannot operate with items whose size is less than two."));
-
+                Optional.ofNullable(numbers)
+                                .flatMap(list ->
+                                        IntStream
+                                                .range(1, numbers.size())
+                                                .boxed()
+                                                .map(i -> numbers.get(i - 1) + numbers.get(i))
+                                                .max(Integer::compareTo)
+                                )
+                        .orElseThrow(InvalidParameterException::new);
     }
 
     public static Integer getMaxSumOfAdjacentNumbersParallelized(final List<Integer> numbers) {
 
-        if (Objects.isNull(numbers)) {
-            throw new InvalidParameterException("Number list cannot be null.");
-        }
-
         return
-                IntStream
-                        .range(1, numbers.size())
-                        .parallel()
-                        .mapToObj(i -> numbers.get(i - 1) + numbers.get(i))
-                        .max(Integer::compareTo)
-                        .orElseThrow(() -> new InvalidParameterException("Cannot operate with items whose size is less than two."));
+                Optional.ofNullable(numbers)
+                        .flatMap(list ->
+                                IntStream
+                                        .range(1, numbers.size())
+                                        .boxed()
+                                        .parallel()
+                                        .map(i -> numbers.get(i - 1) + numbers.get(i))
+                                        .max(Integer::compareTo)
+                        )
+                        .orElseThrow(InvalidParameterException::new);
 
     }
-
 
 }
